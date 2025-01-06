765fc3890feb2ede347deb9dbe1f9149154206c1
Sav <dsouvik141@gmail.com>
2025-01-07 01:13:40 +0530

    Format code using black via manually triggering pre-commit hook via "pre-commit run --all-files" command

--- kobidh/cli.py
+++ kobidh/cli.py
@@ -2,9 +2,11 @@ import click
 from click import echo
 from kobidh.core import some_function
 
+
 @click.group()
 def main():
     pass
+
 
 @main.command()
 def test():
--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='kobidh',
+    name="kobidh",
     version=VERSION,
-    description='CLI tool for automating the deployment process of server-side applications with ease and efficiency',
+    description="CLI tool for automating the deployment process of server-side applications with ease and efficiency",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/Qurtesy/kobidh',
+    url="https://github.com/Qurtesy/kobidh",
     author="Souvik Dey",
     author_email="dsouvik141@gmail.com",
     license="MIT",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.9',
+    python_requires=">=3.9",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
@@ -27,9 +27,9 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
-    entry_points='''
+    entry_points="""
         [console_scripts]
         kobidh=kobidh.cli:main
-    ''',
+    """,
     keywords=["cli", "automation", "deployment", "server-side", "devops"],
 )
